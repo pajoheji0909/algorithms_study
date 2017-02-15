667ebaa6e1d47aadd94dfacf5323bcb088087efd
pajoheji0909 <yiyi9609@naver.com>
2017-02-15 19:06:39 +0900

    Update ch2.cpp

--- ch2.cpp
+++ ch2.cpp
@@ -7,8 +7,8 @@ using namespace std;
 void insertion_sort(int n) {
   int length_arr = n;
   int arr [length_arr];
+  srand((unsigned int)time(NULL));
   for (int i = 0; i < length_arr; i++) {
-    srand((unsigned int)time(NULL));
     arr[i] = rand();
   }
   for (int i = 0; i <length_arr; i++) {
@@ -20,8 +20,9 @@ void insertion_sort(int n) {
     int i = j-1;
     while (i >= 0 && arr[i] > key) {
       arr[i+1] = arr[i];
-      arr[i+1] = key;
+      i = i -1 ;
     }
+      arr[i+1] = key;
   }
   // finish reallocation of array
   for (int i = 0; i <length_arr; i++) {
